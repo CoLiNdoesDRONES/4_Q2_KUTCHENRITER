d9c51064ceeac1b8129ad59816087f1f70d59d30
CoLiNdoesDRONES <148979898+CoLiNdoesDRONES@users.noreply.github.com>
2023-12-04 11:01:40 -0500

    enemy

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float EnemySpeed = 5.0f;
+    public Game[] wayPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
