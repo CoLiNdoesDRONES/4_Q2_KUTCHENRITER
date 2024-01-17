a6adee3332f88ab0cdfc2124438b4cb90be7d9aa
CoLiNdoesDRONES <148979898+CoLiNdoesDRONES@users.noreply.github.com>
2024-01-17 10:15:47 -0500

    win and game over screen done just have to code it

--- Assets/scripts/nextLEVEL.cs
+++ Assets/scripts/nextLEVEL.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class nextLEVEL : MonoBehaviour
-{
+{private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+           SceneManager.LoadScene("level2");
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
